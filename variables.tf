034b40e224092512e79a8e2d05134c2bf540d677
Sami Kuronen <sami.kuronen@fi.ibm.com>
2022-08-10 13:53:36 +0300

    variable examples fix

--- variables.tf
+++ variables.tf
@@ -367,14 +367,13 @@ variable "zone_clusters" {
       nsx_t_managers = true    # Value must "true" for the first cluster
       nsx_t_edges = true       # Value must "true" for the first cluster
       public_ips = 2           # Orders # of Floating IPs for the T0. 
-      overlay_networks = [     # Add networks to be routed on the overlay for the T0 on this cluster. 
       overlay_networks = [     # Add networks to be routed on the overlay for the T0 on mgmt domain/cluster. 
           { name = "customer-overlay", destination = "172.16.0.0/16" },
           { name = "vcf-avn-local-network", destination = "172.27.16.0/24" },
           { name = "avn-x-region-network", destination = "172.27.17.0/24" },
         ]
       vpc_file_shares = []     # Future use.
-    },   
+    },
   }
 }
 
